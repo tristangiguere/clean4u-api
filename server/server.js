de9f2422d576576b2759be39f14de829ac283617
Tristan Giguere <tristan@pool-app.ca>
2022-02-10 13:44:35 -0500

    Initial implementation of authentication

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const invoiceService = require('./routes/invoices');
 const quotationRequestService = require('./routes/requests');
+const authService = require('./routes/auth');
+const authMW = require('./middleware/auth');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/invoices', invoiceService);
-app.use('/api/requests', quotationRequestService);
+app.use('/api/invoices', authMW, invoiceService);
+app.use('/api/requests',authMW, quotationRequestService);
+app.use('/api/auth', authService);
 
 const PORT = 3000;
 
